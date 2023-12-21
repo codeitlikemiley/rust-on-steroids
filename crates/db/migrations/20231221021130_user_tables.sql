0aaf471f79d469a516286c163446c0a981dcb89b
Uriah Galang <codeitlikemiley@gmail.com>
2023-12-21 02:16:04 +0000

    move to correct path

--- crates/db/migrations/20231221021130_user_tables.sql
+++ crates/db/migrations/20231221021130_user_tables.sql
@@ -1,14 +1,14 @@
 -- migrate:up
 
 CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       email VARCHAR NOT NULL UNIQUE,
-                       hashed_password VARCHAR NOT NULL,
-                       reset_password_selector VARCHAR,
-                       reset_password_sent_at TIMESTAMP,
-                       reset_password_validator_hash VARCHAR,
-                       created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-                       updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    hashed_password VARCHAR NOT NULL,
+    reset_password_selector VARCHAR,
+    reset_password_sent_at TIMESTAMP,
+    reset_password_validator_hash VARCHAR,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 INSERT INTO users(email, hashed_password) VALUES('test1@test1.com', 'aasdsaddasad');
@@ -16,14 +16,14 @@ INSERT INTO users(email, hashed_password) VALUES('test2@test1.com', 'aasdsaddasa
 INSERT INTO users(email, hashed_password) VALUES('test3@test1.com', 'aasdsaddasad');
 
 CREATE TABLE sessions (
-                          id SERIAL PRIMARY KEY,
-                          session_verifier VARCHAR NOT NULL,
-                          user_id INT NOT NULL,
-                          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                          otp_code_encrypted VARCHAR NOT NULL,
-                          otp_code_attempts INTEGER NOT NULL DEFAULT 0,
-                          otp_code_confirmed BOOLEAN NOT NULL DEFAULT false,
-                          otp_code_sent BOOLEAN NOT NULL DEFAULT false
+    id SERIAL PRIMARY KEY,
+    session_verifier VARCHAR NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    otp_code_encrypted VARCHAR NOT NULL,
+    otp_code_attempts INTEGER NOT NULL DEFAULT 0,
+    otp_code_confirmed BOOLEAN NOT NULL DEFAULT false,
+    otp_code_sent BOOLEAN NOT NULL DEFAULT false
 );
 
 COMMENT ON TABLE sessions IS 'The users login sessions';
